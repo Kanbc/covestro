c71c0ebdd36b1660de537737e87c0f02de0a9167
Pawarisson <kan.pawarisson@gmail.com>
2017-11-10 14:19:07 +0700

    before to use real material

---
+++ js/app.js
@@ -0,0 +1,35 @@
+new Vue({
+    el: '#app1',
+    methods: {
+        showMessage: function() {
+            this.$Message('Thanks for using AT-UI')
+        }
+    }
+})
+
+new Vue({
+    el: '#app2',
+    methods: {
+        showMessage: function() {
+            this.$Message('Thanks for using AT-UI')
+        }
+    }
+})
+
+new Vue({
+    el: '#app3',
+    methods: {
+        showMessage: function() {
+            this.$Message('Thanks for using AT-UI')
+        }
+    }
+})
+
+new Vue({
+    el: '#app4',
+    methods: {
+        showMessage: function() {
+            this.$Message('Thanks for using AT-UI')
+        }
+    }
+})
