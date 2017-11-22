8833cff1b97b63c2e175f337021a5ea29623228d
Pawarisson <kan.pawarisson@gmail.com>
2017-11-22 14:03:12 +0700

    popup demo done

--- js/app.js
+++ js/app.js
@@ -1,35 +1,22 @@
-new Vue({
-    el: '#app1',
-    methods: {
-        showMessage: function() {
-            this.$Message('Thanks for using AT-UI')
-        }
-    }
-})
+// Popup 1
+var template1 = document.querySelector('#popup-template1') 
+var htmlTip1  = tippy('#btn1',{
+                    html: template1,
+                    placement: 'bottom',
+                    animation: 'fade',
+                    animateFill: false,
+                    duration: [300,100],
+                    arrow: false,
+                    trigger:'click',
+                    interactive: true,
+                    distance:10,
+                    maxWidth: '500px',
+                    interactiveBorder: 0,
+                    theme: 'covestro',
+                });
 
-new Vue({
-    el: '#app2',
-    methods: {
-        showMessage: function() {
-            this.$Message('Thanks for using AT-UI')
-        }
-    }
-})
+// Close Button 1
+function closePop1(){
+    document.querySelector('#btn1')._tippy.hide(75)
+}
 
-new Vue({
-    el: '#app3',
-    methods: {
-        showMessage: function() {
-            this.$Message('Thanks for using AT-UI')
-        }
-    }
-})
-
-new Vue({
-    el: '#app4',
-    methods: {
-        showMessage: function() {
-            this.$Message('Thanks for using AT-UI')
-        }
-    }
-})
